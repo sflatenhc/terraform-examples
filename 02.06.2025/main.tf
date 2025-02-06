4ba9495db13f8e7b5593c699e4ddd2944778254b
Stephen Flaten <stephen.flaten@hennepin.us>
2025-02-06 10:59:47 -0600

    feat: add some locals for testing

--- 02.06.2025/main.tf
+++ 02.06.2025/main.tf
@@ -1,10 +1,10 @@
 locals {
   ## reserved for solution wide "stuff"
 
-  string_upper = "TEST"
+  string_upper = "TEST123"
   string_lower = "test"
 
-  data_file = "${path.module}/data/sample.json"
+  data_file = "${path.module}/data/sample1.json"
   file_data = jsondecode(file(local.data_file))
 
   data_file_set = fileset("${path.module}/data/", "*.json")
@@ -12,5 +12,17 @@ locals {
     for samplefile in local.data_file_set : samplefile =>
     jsondecode(file("${path.module}/data/${samplefile}"))
   }
+
+  vnetcidr = "10.10.10.0/23" ## Normally retrieved from IPAM
+  cluster_cidr          = cidrsubnet(local.vnetcidr, 1, 0)
+  private_endpoint_cidr = cidrsubnet(local.vnetcidr, 4, 8)
+  agw_cidr              = cidrsubnet(local.vnetcidr, 4, 9)
+  github_runners_cidr   = cidrsubnet(local.vnetcidr, 3, 5)
+
+
 }
+
+
+
+
 
