b4832318e7a3c63ccc4e4392afdd51ca09632125
Stephen Flaten <stephen.flaten@hennepin.us>
2025-02-06 09:48:38 -0600

    feat: add some locals for testing

--- 02.06.2025/main.tf
+++ 02.06.2025/main.tf
@@ -1,3 +1,16 @@
 locals {
-    ## reserved for solution wide "stuff"
+  ## reserved for solution wide "stuff"
+
+  string_upper = "TEST"
+  string_lower = "test"
+
+  data_file = "${path.module}/data/sample.json"
+  file_data = jsondecode(file(local.data_file))
+
+  data_file_set = fileset("${path.module}/data/", "*.json")
+  file_set_data = {
+    for samplefile in local.data_file_set : samplefile =>
+    jsondecode(file("${path.module}/data/${samplefile}"))
+  }
 }
+
